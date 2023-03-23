44bdd18bf87e62bcc91daad1fc8067bb398a1d3a
Amy Snarr <amy.snarr16@gmail.com>
2023-03-22 19:22:09 -0600

    can display from container

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+
+export default function App() {
+    console.log('Rendering App');
+    return <PortfolioContainer />
+}
