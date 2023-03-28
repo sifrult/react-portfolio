e9a00d2e0b03217559ed5188f43eed5409199c05
Amy Snarr <amy.snarr16@gmail.com>
2023-03-27 18:45:57 -0600

    can download resume

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,39 +2,44 @@ import React from 'react';
 
 export default function Nav({currentPage, handlePageChange}) {
     return (
-        <ul className='header'>
-            <li>
-                <a
-                href="#home"
-                onClick={() => handlePageChange('Home')}
-                className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-                >About Me
-                </a>
-            </li>
-            <li>
-                <a
-                href="#portfolio"
-                onClick={() => handlePageChange('Portfolio')}
-                className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-                >Portfolio
-                </a>
-            </li>
-            <li>
-                <a
-                href="#contact"
-                onClick={() => handlePageChange('Contact')}
-                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                >Contact
-                </a>
-            </li>
-            <li>
-                <a
-                href="#resume"
-                onClick={() => handlePageChange('Resume')}
-                className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-                >Resume
-                </a>
-            </li>
-        </ul>
+        <div>
+            <h1>
+                Amy Snarr
+            </h1>
+            <ul className='header'>
+                <li>
+                    <a
+                    href="#home"
+                    onClick={() => handlePageChange('Home')}
+                    className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+                    >About Me
+                    </a>
+                </li>
+                <li>
+                    <a
+                    href="#portfolio"
+                    onClick={() => handlePageChange('Portfolio')}
+                    className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+                    >Portfolio
+                    </a>
+                </li>
+                <li>
+                    <a
+                    href="#contact"
+                    onClick={() => handlePageChange('Contact')}
+                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                    >Contact
+                    </a>
+                </li>
+                <li>
+                    <a
+                    href="#resume"
+                    onClick={() => handlePageChange('Resume')}
+                    className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                    >Resume
+                    </a>
+                </li>
+            </ul>
+        </div>
     )
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
+
 export default function Resume() {
-    return (
-        <div>
-            <p>
-                RESUME
-            </p>
-        </div>
-    )
+
+    const onButtonClick = () => {
+        const fileURL = '/Snarr_Resume.pdf';
+        const a = document.createElement('a');
+        a.href = fileURL;
+        a.download = '/Snarr_Resume.pdf';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      }
+
+      return (
+        <>
+          <center>
+            <h1>Welcome to Geeks for Geeks</h1>
+            <h3>Click on below button to download PDF file</h3>
+            <button onClick={onButtonClick}>
+              Download PDF
+            </button>
+          </center>
+        </>
+      );
 }
