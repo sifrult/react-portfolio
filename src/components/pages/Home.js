bd10f7210eba529fbbffd400b7a1fcfe77752ae6
Amy Snarr <amy.snarr16@gmail.com>
2023-03-27 21:00:20 -0600

    about me page has responsive css

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,15 +2,10 @@ import React from 'react';
 import avatar from '../../assets/avatar.png';
 
 export default function Home() {
-
-    const contentStyle = {border: '1px solid black', padding: '15px', display: 'flex', flexDirection: 'column', alignItems:"center", textAlign: 'center'}
-
-    const avatarStyle = {width: '50%'}
-
     return (
-        <div style={contentStyle}>
-            <img src={avatar} alt='avatar' style={avatarStyle}></img>
-            <p>
+        <div className='contentStyle'>
+            <img src={avatar} alt='avatar' className='avatarStyle'></img>
+            <div><p>
                 Hello! My name is Amy! I have a passion for math, coding, and all things logic. I love to crochet and cross stitch, and do puzzles with my two daughters.
             </p>
             <p>
@@ -22,9 +17,10 @@ export default function Home() {
                 <br />Meal: Pozole
                 <br />Fruit: Mango
                 <br />Fast food: Popeye's
+                <br />Place: Williamsburg, VA
                 <br />Musician: Taylor Swift
                 <br />Animal: Owl
-            </p>
+            </p></div>
         </div>
     )
 }
