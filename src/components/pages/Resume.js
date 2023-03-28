6c8513228417abdab484a8db49c74400a0823403
Amy Snarr <amy.snarr16@gmail.com>
2023-03-27 18:50:09 -0600

    resume section contains list of my proficiencies

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 
 export default function Resume() {
-
     const onButtonClick = () => {
         const fileURL = '/Snarr_Resume.pdf';
         const a = document.createElement('a');
@@ -11,17 +10,24 @@ export default function Resume() {
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
-      }
+    }
 
-      return (
-        <>
-          <center>
-            <h1>Welcome to Geeks for Geeks</h1>
-            <h3>Click on below button to download PDF file</h3>
+    return (
+        <div>
+            <ul>
+                <li><img src="https://skillicons.dev/icons?i=html" title="HTML" alt="HTML"/></li>
+                <li><img src="https://skillicons.dev/icons?i=css" title="CSS" alt="CSS"/></li>
+                <li><img src="https://skillicons.dev/icons?i=bootstrap" title="Bootstrap" alt="Bootstrap"/></li>
+                <li><img src="https://skillicons.dev/icons?i=js" title="JavaScript" alt="JavaScript"/></li>
+                <li><img src="https://skillicons.dev/icons?i=jquery" title="JQuery" alt="JQuery"/></li>
+                <li><img src="https://skillicons.dev/icons?i=mongodb" title="MongoDB" alt="MongoDB"/></li>
+                <li><img src="https://skillicons.dev/icons?i=nodejs" title="NodeJS" alt="NodeJS"/></li>
+                <li><img src="https://skillicons.dev/icons?i=react" title="React" alt="React"/></li>
+                <li><img src="https://skillicons.dev/icons?i=vscode" title="VSCode" alt="VSCode"/></li>
+            </ul>
             <button onClick={onButtonClick}>
               Download PDF
             </button>
-          </center>
-        </>
-      );
+        </div>
+    );
 }
